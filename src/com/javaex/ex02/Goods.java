762af70a10f9208b9d0284cbd620239a3096e7c4
유승원 <yoosw4595@gmail.com>
2024-03-27 09:37:16 +0900

    24.3.27 (1)

--- src/com/javaex/ex02/Goods.java
+++ src/com/javaex/ex02/Goods.java
@@ -3,39 +3,53 @@ package com.javaex.ex02;
 public class Goods {
 	private String name;  // 상품명
 	private int price;    // 가격
-	
-	public Goods(String name) {
-		this.name = name;
-		System.out.println("name 필드 초기화 생성자");
-	}
-	// 전체 필드 생성자
-	public Goods(String name, int price) {
-		// 객체 초기화를 담당
-		this(name); // -> Goods(String name) 생성자 호출
+	private String cupname;  // 상품명
+	private int cupprice;
+		
+	public Goods(String name, int price, String cupname, int cupprice) {		
+		this.name = name; 
 		this.price = price;
-		System.out.println("전체 필드 초기화 생성자");
+		this.cupname = cupname; 
+		this.cupprice = cupprice;
 	}
-	// getter Setter
-	// Setter가 없으면 Read Only 필드를 만들 수 있고
-	// getter/setter 모두 없으면 프라이빗 필드가 된다
-	public String getName() {
-		// 메서드 영역
-	return name;
+	
+	public String getName() {		
+		return name;
 	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
+	
+	public String getCupname() {		
+		return cupname;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	private void setCupname(String cupname) {
+		this.cupname = cupname;
+	}
+
 	public int getPrice() {
 		return price;
 	}
+	public int getCupprice() {
+		return cupprice;
+	}
 	
+	private void setPrice(int price) {
+		this.price = price;
+	}
+	private void setCupprice(int cupprice) {
+		this.cupprice = cupprice;
+	}
+		
 	
-	
-	// 일반 메서드
 	public void showInfo() {
 		System.out.println("상품명: " + name + ", 가격: " + price);
 	}
-	System.out.println("상품명: 머그컵  가격: 2000");
+	public void showInfo1() {
+		System.out.println("상품명: " + cupname + ", 가격: " + cupprice);
+	}
+	
 	
 
 }
